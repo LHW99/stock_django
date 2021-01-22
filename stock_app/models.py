329099780376765259ba1bb996844f409d23ef9a
haiduk <hwlee91@gmail.com>
2021-01-21 22:56:41 -0500

    mapped basic url

--- stock_app/models.py
+++ stock_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
-# Create your models here.
+class Ticker(models.Model):
+  tick = models.CharField(max_length = 6)
+
+  def get_absolute_url(self):
+    return reverse("ticker-detail", kwargs={"pk": self.pk})
+
+  def __str__(self):
+    return self.tick
+
+class Price(models.Model):
+  cost = models.DecimalField(max_digits=6, decimal_places=2)
+  ticker = models.ForeignKey('Ticker', on_delete=models.SET_NULL, null=True)
+
+  def __str__(self):
+    return self.cost
