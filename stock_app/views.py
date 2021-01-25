015158cdbd3a2e979765e8a9a2d7c35057cc6f24
haiduk <hwlee91@gmail.com>
2021-01-24 22:21:41 -0500

    api simple call works

--- stock_app/views.py
+++ stock_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from keys import *
+import requests
+import pyEX as p
+#api_call = p.Client(api_token=CLOUD_API_KEY, version='sandbox')
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  response = requests.get(f'https://sandbox.iexapis.com/stable/stock/AAPL/quote?token={CLOUD_API_KEY}')
+  data = response.json()
+  return render(request, 'index.html', {
+    'symbol': data['symbol'],
+    'latestPrice': data['latestPrice']
+  })
