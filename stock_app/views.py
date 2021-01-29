a3cd3af65f337df62394500b4be9fb1a721331a5
haiduk <hwlee91@gmail.com>
2021-01-29 04:25:02 -0500

    added some other stats

--- stock_app/views.py
+++ stock_app/views.py
@@ -6,19 +6,28 @@ import pyEX as p
 #api_call = p.Client(api_token=CLOUD_API_KEY, version='sandbox')
 
 def index(request):
+  symbol = 'AAPL'
+  response = requests.get(f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote,advanced-stats&token={CLOUD_API_KEY}')
+  data = response.json()
+
   if request.method == 'GET':
     try:
       ticker = request.GET['ticker_search']
       symbol = ticker.upper()
-      response = requests.get(f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote,news,chart&token={CLOUD_API_KEY}')
-      #response = requests.get(f'https://sandbox.iexapis.com/stable/stock/{symbol}/batch?types=quote&token={CLOUD_API_KEY}')
-      #response = requests.get(f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={CLOUD_API_KEY}')
+      response = requests.get(f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote,advanced-stats&token={CLOUD_API_KEY}')
       data = response.json()
 
       return render(request, 'index.html', {
+      'companyName': data[symbol]['quote']['companyName'],
       'symbol': symbol,
       'latestPrice': data[symbol]['quote']['latestPrice'],
-      'error_message': ''
+      'peRatio': data[symbol]['quote']['peRatio'],
+      'marketCap': data[symbol]['quote']['marketCap'],
+      'week52High': data[symbol]['quote']['week52High'],
+      'week52Low': data[symbol]['quote']['week52Low'],
+      'priceToBook': data[symbol]['advanced-stats']['priceToBook'],
+      'priceToSales': data[symbol]['advanced-stats']['priceToSales'],
+      #'error_message': ''
       })
     except:
       print('try again')
