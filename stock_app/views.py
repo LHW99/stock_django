83d14d0cb844f89e468c474da22713a5f942376d
haiduk <hwlee91@gmail.com>
2021-02-08 05:02:32 -0500

    formatted front page/header

--- stock_app/views.py
+++ stock_app/views.py
@@ -9,19 +9,23 @@ def index(request):
     try:
       ticker = request.GET['ticker_search']
       symbol = ticker.upper()
-      response = requests.get(f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote,advanced-stats&token={CLOUD_API_KEY}')
+      response = requests.get(f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol}&types=quote,stats,advanced-stats&token={CLOUD_API_KEY}')
       data = response.json()
 
       return render(request, 'index.html', {
       'companyName': data[symbol]['quote']['companyName'],
       'symbol': f'({symbol})',
       'latestPrice': f"${data[symbol]['quote']['latestPrice']}",
-      'peRatio': data[symbol]['quote']['peRatio'],
-      'marketCap': f"{data[symbol]['quote']['marketCap']/1000000000}",
-      'week52High': f"${data[symbol]['quote']['week52High']}",
-      'week52Low': f"${data[symbol]['quote']['week52Low']}",
-      'priceToBook': data[symbol]['advanced-stats']['priceToBook'],
-      'priceToSales': data[symbol]['advanced-stats']['priceToSales'],
+      'marketCap': f"Market Cap (Billions): {data[symbol]['quote']['marketCap']/1000000000:.2f}",
+      'month1ChangePercent': f"1-Month Percentage Change: {data[symbol]['stats']['month1ChangePercent']*100:.2f}%",
+      'month3ChangePercent': f"3-Month Percentage Change: {data[symbol]['stats']['month3ChangePercent']*100:.2f}%",
+      'month6ChangePercent': f"6-Month Percentage Change: {data[symbol]['stats']['month6ChangePercent']*100:.2f}%",
+      'year1ChangePercent': f"1-Year Percentage Change: {data[symbol]['stats']['year1ChangePercent']*100:.2f}%",  
+      'week52High': f"52-Week High: ${data[symbol]['quote']['week52High']}",
+      'week52Low': f"52-Week Low: ${data[symbol]['quote']['week52Low']}",
+      'peRatio': f"Price-to-Earnings Ratio: {data[symbol]['quote']['peRatio']}",
+      'priceToBook': f"Price-to-Book Ratio: {data[symbol]['advanced-stats']['priceToBook']}",
+      'priceToSales': f"Price-to-Sales Ratio: {data[symbol]['advanced-stats']['priceToSales']}",
       })
     except:
       return render(request, 'index.html')
