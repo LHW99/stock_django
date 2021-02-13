170b20f750b07b32d078ecfd045f3ef7e613ec48
haiduk <hwlee91@gmail.com>
2021-02-13 02:01:07 -0500

    update pro settings

--- stock_django/settings/production_settings.py
+++ stock_django/settings/production_settings.py
@@ -11,3 +11,7 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = os.environ['SECRET_KEY']
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
